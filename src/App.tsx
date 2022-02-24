c417b2bde75a2b5048fb2bf8d27750ad2a26753b
Lilia Ivanska <ivanskalilia16@gmail.com>
2022-02-24 02:04:38 +0200

    Added linter, postcss-loader, file-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./style.scss";
 const App = () => (
   <>
     <p>Custom configs</p>
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
@@ -23,10 +31,14 @@ module.exports = {
           loader: "ts-loader",
         },
       },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
     ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx", ".scss"],
   },
   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
 };
